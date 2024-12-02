ab1c5dedf8fe668cdc86e5ee0029127e309ee462
Rohan <rohanrc101@gmail.com>
2024-12-02 11:20:28 +0300

    [FEAT] creating playingcard model

---
+++ modele/PlayingCard.java
@@ -0,0 +1,22 @@
+class PlayingCard{
+    private Rank rank;
+    private Suit suit;
+    private boolean faceUp;
+
+    public PlayingCard(Rank rank,Suit suit){
+        this.rank=rank;
+        this.suit=suit;
+    }
+
+    public Rank getRank(){
+        return this.rank;
+    }
+
+    public Suit getSuit(){
+        return this.suit;
+    }
+
+    public boolean isFaceUp(){
+        return this.faceUp;
+    }
+}
