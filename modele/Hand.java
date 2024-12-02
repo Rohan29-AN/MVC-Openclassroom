beb572959f8c2817dd6672c12fd76a42768e6ea4
Rohan <rohanrc101@gmail.com>
2024-12-02 11:24:11 +0300

    [FEAT] adding hand class

---
+++ modele/Hand.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Hand{
+    List<PlayingCard> cards;
+
+    public Hand(){
+        this.cards=new ArrayList<PlayingCard>();
+    }
+
+    public void addCard(PlayingCard pc){
+        this.cards.add(pc);
+    }
+
+    public PlayingCard getCard(int index){
+        return this.cards.get(index);
+    }
+
+    public PlayingCard removeCard(){
+        return this.cards.remove(0);
+    }
+}
