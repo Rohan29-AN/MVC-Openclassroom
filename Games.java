720c9b1d36468653ea14ed80019276c420637f47
Rohan <rohanrc101@gmail.com>
2024-12-05 22:54:47 +0300

    [REFACTOR] factory design pattern

--- Games.java
+++ Games.java
@@ -3,10 +3,11 @@ import controleur.*;
 import vue.*;
 import modele.*;
 import game.*;
+import modele.DeckFactory.DeckType;
 
 public class Games{
     public static void main(String args[]) {
-		GameController gc = new GameController(new CommandLineView(),new Deck(),new HighCardGameEvaluator());
+		GameController gc = new GameController(new CommandLineView(),DeckFactory.makeDeck(DeckType.NORMAL),new HighCardGameEvaluator());
 		gc.run();
 	}
 }
