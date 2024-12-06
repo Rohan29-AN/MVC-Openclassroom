8cd8bfe21e21287ec9b8c524f1c4d60217e90407
Rohan <rohanrc101@gmail.com>
2024-12-06 21:19:43 +0300

    [FEAT] simple message

--- Games.java
+++ Games.java
@@ -11,5 +11,6 @@ public class Games{
 		gc.run();
 		System.out.println("Nothing special, I'm tired");
 		System.out.println("But I don't want to give up");
+		System.out.println("The purpose of these sout is to maintain my coding streak every day");
 	}
 }
