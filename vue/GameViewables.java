b71056726251b5842b3c7137e5956605f61524ab
Rohan <rohanrc101@gmail.com>
2024-12-08 21:55:37 +0300

    [FIX] restore accidentally deleted controller class

--- vue/GameViewables.java
+++ vue/GameViewables.java
@@ -1,64 +1,65 @@
 package vue;
-
 import java.util.*;
-import controleur.*;
+import controleur.*; 
 
-public class GameViewables implements GameViewable {
-    List<GameViewable> views;
 
-    public GameViewables() {
-        this.views = new ArrayList<GameViewable>();
+public class GameViewables implements GameViewable{
+    List<GameViewable>  views;
+
+
+    public GameViewables(){
+        this.views=new ArrayList<GameViewable>();
     }
 
-    public void addView(GameViewable view) {
+    public void addView(GameViewable view){
         this.views.add(view);
     }
 
-    void setController(GameController gameController) {
-        for (GameViewable view : this.view) {
+     void setController(GameController gameController){
+        for(GameViewable view: this.view){
             this.view.setController(gameController);
         }
-    }
-
-    void promptForPlayerName() {
-        for (GameViewable view : this.view) {
+     }
+  
+    void promptForPlayerName(){
+         for(GameViewable view: this.view){
             this.view.promptForPlayerName();
         }
     }
-
-    void promptForNewGame() {
-        for (GameViewable view : this.view) {
+  
+    void promptForNewGame(){
+         for(GameViewable view: this.view){
             this.view.promptForNewGame();
         }
     }
-
-    void promptForFlip() {
-        for (GameViewable view : this.view) {
+  
+    void promptForFlip(){
+         for(GameViewable view: this.view){
             this.view.promptForFlip();
         }
     }
-
-    void showWinner(String playerName) {
-        for (GameViewable view : this.view) {
+  
+    void showWinner(String playerName){
+          for(GameViewable view: this.view){
             this.view.showWinner(playerName);
         }
     }
-
-    void showPlayerName(int playerIndex, String playerName) {
-        for (GameViewable view : this.view) {
-            this.view.showPlayerName(playerIndex, playerName);
+  
+    void showPlayerName(int playerIndex,String playerName){
+           for(GameViewable view: this.view){
+            this.view.showPlayerName(playerIndex,playerName);
         }
     }
-
-    void showFaceDownCardForPlayer(int i, String playerName) {
-        for (GameViewable view : this.view) {
-            this.view.showFaceDownCardForPlayer(i, playerName);
+  
+    void showFaceDownCardForPlayer(int i,String playerName){
+         for(GameViewable view: this.view){
+            this.view.showFaceDownCardForPlayer(i,playerName);
         }
     }
-
-    void showCardForPlayer(int i, String playerName, int rank, String suit) {
-        for (GameViewable view : this.view) {
-            this.view.showCardForPlayer(i, playerName, rank, suit);
+  
+    void showCardForPlayer(int i,String playerName,int rank,String suit){
+         for(GameViewable view: this.view){
+            this.view.showCardForPlayer(i,playerName,rank,suit);
         }
     }
 
