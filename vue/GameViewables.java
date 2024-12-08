7bbbb845358c720a0e39b7032902035dd5425b0c
Rohan <rohanrc101@gmail.com>
2024-12-08 21:21:52 +0300

    [CHORE] application of the design pattern composite

--- vue/GameViewables.java
+++ vue/GameViewables.java
@@ -1,4 +1,50 @@
 package vue;
+import java.util.*;
+import controleur.*; 
+
+
 public class GameViewables implements GameViewable{
-    
+    List<GameViewable>  views;
+
+
+    public GameViewables(){
+        this.views=new ArrayList<GameViewable>();
+    }
+
+    public void addView(GameViewable view){
+        this.views.add(view);
+    }
+
+     void setController(GameController gameController){
+
+     }
+  
+    void promptForPlayerName(){
+
+    }
+  
+    void promptForNewGame(){
+
+    }
+  
+    void promptForFlip(){
+
+    }
+  
+    void showWinner(String playerName){
+
+    }
+  
+    void showPlayerName(int playerIndex,String playerName){
+
+    }
+  
+    void showFaceDownCardForPlayer(int i,String playerName){
+
+    }
+  
+    void showCardForPlayer(int i,String playerName,int rank,String suit){
+
+    }
+
 }
